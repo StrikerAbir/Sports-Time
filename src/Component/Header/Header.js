31faa6b3fe3f5c587efb6d64259231daaf9995cb
Abir Hasan <cse.170201063@gmail.com>
2022-09-28 22:37:03 +0600

    tailwind added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="cart">
+            <h1>dfaosdfjlsdfl</h1>
+        </div>
+    );
+};
+
+export default Header;
